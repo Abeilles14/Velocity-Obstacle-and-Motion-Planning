dab9f3cde0f57c2ffd09ca8960a8d57b2c58a388
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2021-11-06 20:39:55 -0700

    2 arm state machines work!

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# STATE MACHINE FOR 3D PICK AND PLACE SIMULATION
+# 3D PICK AND PLACE SIMULATION
 import numpy as np
+import time
+import logging
 from numpy.linalg import norm
 from math import *
 from matplotlib import pyplot as plt
@@ -7,7 +9,6 @@ from matplotlib.patches import Polygon
 from random import random
 from scipy.spatial import ConvexHull
 from matplotlib import path
-import time
 from mpl_toolkits import mplot3d
 from enum import Enum
 
@@ -19,12 +20,17 @@ from arm import Arm
 from RRTStar import RRTStar
 from arm_state_machine import ArmStateMachine, ArmState
 
+logger = logging.getLogger(__name__)
+logging.basicConfig()
+logger.setLevel(logging.INFO)
+
 ### CONSTANTS ###
 pause_time = 0.0005
-ARM1_HOME_POS = np.array([0.0, 0.0, 0.0])
+ARM1_HOME_POS = np.array([0.0, 0.15, 0.0])
+ARM2_HOME_POS = np.array([0.0, -0.15, 0.0])
 OBJ1 = np.array([0.0, 1.0, 2.5])
+OBJ2 = np.array([0.0, -1.0, 2.5])
 BOWL =  np.array([2, 0.0, 1.0])
-
 
 ### PARAMETERS ###
 show_RRT = False
@@ -62,19 +68,31 @@ def main():
     for obstacle in obstacles: obstacle.draw(ax)
 
     ### SET UP OBJECTS ###
+    # bowl
     ax.scatter3D(BOWL[0], BOWL[1], BOWL[2], color='red', s=800)
     # TODO: put in pick_and_place sm
     ax.scatter3D(ARM1_HOME_POS[0], ARM1_HOME_POS[1], ARM1_HOME_POS[2], color='blue', s=100)
-    ax.scatter3D(OBJ1[0], OBJ1[1], OBJ1[2], color='green', s=100)
+    ax.scatter3D(OBJ1[0], OBJ1[1], OBJ1[2], color='#99ccff', s=100)
 
-    arm1 = Arm("PSM1", 5, ARM1_HOME_POS, OBJ1)
-    obj1 = Object("OBJ1", arm1, OBJ1)
+    ax.scatter3D(ARM2_HOME_POS[0], ARM2_HOME_POS[1], ARM2_HOME_POS[2], color='green', s=100)
+    ax.scatter3D(OBJ2[0], OBJ2[1], OBJ2[2], color='#99ff99', s=100)
+
+    arm1 = Arm(name="PSM1", velocity=5, position=ARM1_HOME_POS, destination=OBJ1)
+    arm2 = Arm(name="PSM2", velocity=5, position=ARM2_HOME_POS, destination=OBJ2)
+    obj1 = Object(name="OBJ1", arm=arm1, position=OBJ1)
+    obj2 = Object(name="OBJ2", arm=arm2, position=OBJ2)
     arm1_sm = ArmStateMachine(ax, obstacles, arm1, obj1, BOWL)
+    arm2_sm = ArmStateMachine(ax, obstacles, arm2, obj2, BOWL)
 
     # RRTStar(ax, obstacles, ARM1_HOME_POS, OBJ1)
 
-    while (arm1_sm.state != ArmState.DONE): #should be HOME
+    logger.info("Pick and Place Simulation Start")
+
+    while (arm1_sm.state != ArmState.HOME) or (arm2_sm.state != ArmState.HOME): #should be HOME
         arm1_sm.run_once()
+        arm2_sm.run_once()
+
+    logger.info("Pick and Place Simulation End")
 
 if __name__ == '__main__':
     main()
